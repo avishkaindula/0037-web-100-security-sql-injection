3f3492e3a7a6bb0406d274617cac1fb9c32684f5
Avishka Indula <avishkainduladev@gmail.com>
2022-11-09 13:40:48 +0530

    using ? for user inputs and the multipleStatements parameter's default false value

--- data/database.js
+++ data/database.js
@@ -5,7 +5,16 @@ const pool = mysql.createPool({
   database: 'security',
   user: 'root',
   password: 'pass mysql8',
-  multipleStatements: true
+  // multipleStatements: true
+  // By default the multipleStatements value is false.
+  // So by default, the mysql2 package won't accept sql queries as input values more than once.
+  // So even if we use an unsafe method where the users can inject sql queries as
+  // a second sql statement inside user input fields, this built in protection will
+  // automatically ignore that sql query passed by user through that input field.
+  // We've set this value to true before just for demo purposes.
+  // So all this means, nowadays it is so hard to perform sql injection attacks for modern
+  // back-ends like this. Because, there's multiple layers of protections built in 
+  // against sql injection attacks.
 })
 
 module.exports = pool;
--- routes/discussion.js
+++ routes/discussion.js
@@ -12,16 +12,22 @@ router.get('/discussion', async function (req, res) {
   let filter = '';
 
   if (req.query.author) {
-    filter = `WHERE author = "${req.query.author}"`; // Indula"; DROP TABLE comments; SELECT * FROM comments WHERE author = "Janith
+    // filter = `WHERE author = "${req.query.author}"`; // Indula"; DROP TABLE comments; SELECT * FROM comments WHERE author = "Janith
     // If we insert this query inside the find comments by author search bar, the table will be dropped.
     // This is how the attackers perform SQL injection attacks.
+    // We need to use question marks instead of back-ticks when accepting user inputs.
+    // That way, it will provide us a built in protection against sql injection attacks.
+    // Because those values will be automatically escaped and will not be parsed as sql queries.
+    filter = `WHERE author = ?`;
   }
 
   const query = `SELECT * FROM comments ${filter}`;
 
   console.log(query);
 
-  const [comments] = await db.query(query);
+  // const [comments] = await db.query(query);
+  const [comments] = await db.query(query, [req.query.author]);
+  // Now this will give us the necessary protection we need against sql injection attacks.
 
   res.render('discussion', { comments: comments });
 });
